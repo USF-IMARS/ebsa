c0af0d2dd39694273540dc0d8825d1e04b9ba20c
7yl4r <murray.tylar@gmail.com>
2019-12-07 12:47:22 -0500

    modify to work for gulf of mexico

--- script.R
+++ script.R
@@ -11,11 +11,20 @@ source("lib.R")
 
 # config
 
-xlim <- c(-45, 51)
-ylim <- c(20, 90)
+# FL keys (ish)
+# xlim <- c(-81.25, -80.25)
+# ylim <- c(24.5, 25.5)
+
+# gom:
+#xlim <- c(-98, -79)
+#ylim <- c(18, 31)
+
+xlim <- c(-130, -60)
+ylim <- c(18, 50)
+
 datafile_name <- "taxonomy_grouped_20190827"
 shapefile_name <- "ne_atlantic.shp"
-res <- 9
+res <- 5
 
 # construct discrete global grid system
 # see https://github.com/r-barnes/dggridR for grid resolutions
@@ -28,16 +37,21 @@ dir.create("temp")
 url <- paste0("https://download.obis.org/export/", datafile_name, ".zip")
 zip_path <- paste0("temp/", datafile_name, ".zip")
 csv_path <- paste0("temp/", datafile_name, ".csv")
+path <- "temp/taxonomy_grouped_20190827.csv"
 if (!file.exists(path)) {
+  print("downloading file...")
   download.file(url, zip_path)
   unzip(zipfile = zip_path, exdir = "temp")
 }
+print("reading csv...")
 all <- read.csv(csv_path, stringsAsFactors = FALSE)
+print("trimming df...")
 df <- all %>%
   filter(decimallongitude >= xlim[1] & decimallongitude <= xlim[2] & decimallatitude >= ylim[1] & decimallatitude <= ylim[2])
 
 # add cell IDs, calculate metrics, add polygons
 
+print("calculating...")
 metrics <- df %>%
   add_cell(dggs) %>%
   calc(50) %>%
@@ -46,10 +60,11 @@ metrics <- df %>%
 # write to shapefile
 
 dir.create("shapefiles")
-st_write(metrics, "shapefiles/ne_atlantic.shp", delete_layer = TRUE)  
+st_write(metrics, "shapefiles/ne_atlantic.shp", delete_layer = TRUE)
 
 # plot
 
+print("mapping...")
 es <- create_map(metrics, "es", xlim, ylim)
 n <- create_map(metrics, "n", xlim, ylim, trans = "log10")
 sp <- create_map(metrics, "sp", xlim, ylim, trans = "log10")
@@ -57,8 +72,13 @@ shannon <- create_map(metrics, "shannon", xlim, ylim)
 simpson <- create_map(metrics, "simpson", xlim, ylim)
 hill_1 <- create_map(metrics, "hill_1", xlim, ylim, trans = "log10")
 
+print("plotting...")
 figure <- ggarrange(n, sp, es, shannon, simpson, hill_1, ncol = 3, nrow = 2)
 figure
 
-ggsave(filename = "metrics.png", height = 7, width = 12, dpi = 300, scale = 1.4)
-
+print("saving...")
+ggsave(
+    filename = paste0("metrics_res-", res, ".png"),
+    height = 7, width = 12, dpi = 300, scale = 1.4
+)
+print("done.")
